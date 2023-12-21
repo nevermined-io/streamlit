d4cb928fa02b696b68808bd27745a71b5cc99e57
Rodolphe Marques <marques.rodolphe@gmail.com>
2023-12-21 11:14:14 +0100

    feat: update webservice did

--- nevermined-app.py
+++ nevermined-app.py
@@ -26,7 +26,7 @@ def ask_elvis(prompt, url, token):
         return "There was a problem with the prompt"
 
 
-st.markdown('<iframe src="https://goerli.nevermined.one/streamlit?did=did:nv:e3df98ba99db7340494aabae193b2b212b80ea6fcc152d9e333624c103f6da86" style="border-radius: 10px; width: 100%;" />', unsafe_allow_html=True)
+st.markdown('<iframe src="http://localhost:3000/streamlit?did=did:nv:257a5999aa3bc96510a931184dc8afaa42dbb8f4e61ac47a03bbb546edd1b860" style="border-radius: 10px; width: 100%;" />', unsafe_allow_html=True)
 
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
