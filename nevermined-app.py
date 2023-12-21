015f4485f6a055d5363e84a522d58a69e1ccdad3
Iurii ᚾ <iurii.tankievych@outlook.com>
2023-12-21 11:30:18 +0000

    revert

--- nevermined-app.py
+++ nevermined-app.py
@@ -26,7 +26,7 @@ def ask_elvis(prompt, url, token):
         return "There was a problem with the prompt"
 
 
-st.markdown('<iframe sandbox="allow-scripts allow-top-navigation" src="https://goerli.nevermined.one/streamlit?did=did:nv:257a5999aa3bc96510a931184dc8afaa42dbb8f4e61ac47a03bbb546edd1b860" style="border-radius: 10px; width: 100%;" />', unsafe_allow_html=True)
+st.markdown('<iframe sandbox="allow-scripts allow-same-origin allow-top-navigation" src="https://goerli.nevermined.one/streamlit?did=did:nv:257a5999aa3bc96510a931184dc8afaa42dbb8f4e61ac47a03bbb546edd1b860" style="border-radius: 10px; width: 100%;" />', unsafe_allow_html=True)
 
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
